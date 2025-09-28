ba98419cf34bea6800c012b157d21f39e766e522
Carlos Huallpa <167722493+Huallpacl@users.noreply.github.com>
2025-09-28 01:29:18 -0300

    API CRUD para Médicos implementada

---
+++ backend/medicos/serializers.py
@@ -0,0 +1,8 @@
+# backend/medicos/serializers.py
+from rest_framework import serializers
+from .models import Medico
+
+class MedicoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Medico
+        fields = '__all__'
