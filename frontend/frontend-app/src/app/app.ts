82009eda84e0c1d38cd258cfb77d2c6f361e78b3
Carlos Huallpa <167722493+Huallpacl@users.noreply.github.com>
2025-09-29 02:20:54 -0300

    Hotfix: Eliminación de modulos usados en versiones anteriores

--- frontend/frontend-app/src/app/app.ts
+++ frontend/frontend-app/src/app/app.ts
@@ -1,16 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-
-import { ClientesList } from './components/clientes-list/clientes-list';
-import { ClientesForm } from './components/clientes-form/clientes-form';
 
 @Component({
   selector: 'app-root',
   standalone: true,              // 👈 marca como standalone
-  imports: [RouterOutlet, RouterModule, ],
+  imports: [RouterOutlet, ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('frontend-app');
 }
