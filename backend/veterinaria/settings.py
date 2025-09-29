43689192590c5a92a92aaa4d4ef5ffd1536a71ec
Carlos Huallpa <167722493+Huallpacl@users.noreply.github.com>
2025-09-29 18:34:20 -0300

    Hotfix: Configuracion de settings.py para permitir cross origin

--- backend/veterinaria/settings.py
+++ backend/veterinaria/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     "mascotas",
     "medicos",
     "turnos",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware", 
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -108,6 +110,8 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Static files (CSS, JavaScript, Images)
