c5e05fabc7a1a0bf18b6d074438ef58cc27e7729
Carlos Huallpa <167722493+Huallpacl@users.noreply.github.com>
2025-09-29 07:36:28 -0300

    Hotfix: Eliminación de modulos redundantes usados en versiones anteriores

--- frontend/frontend-app/src/app/app.routes.ts
+++ frontend/frontend-app/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { ClientesList } from './components/clientes-list/clientes-list';
 import { ClientesForm } from './components/clientes-form/clientes-form';
+import { MascotasList } from './components/mascotas-list/mascotas-list';
+import { MascotasForm } from './components/mascotas-form/mascotas-form';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'clientes-list', pathMatch: 'full' },
   { path: 'clientes-list', component: ClientesList },
   { path: 'clientes-form', component: ClientesForm },
   { path: 'clientes-form/:id', component: ClientesForm },
+  { path: 'mascotas-list', component: MascotasList },
+  { path: 'mascotas-form', component: MascotasForm },
+  { path: 'mascotas-form/:id', component: MascotasForm },
 ];
--- frontend/frontend-app/src/app/app.ts
+++ frontend/frontend-app/src/app/app.ts
@@ -1,10 +1,11 @@
+// frontend-app/src/app/app.ts
 import { Component} from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,              // 👈 marca como standalone
-  imports: [RouterOutlet, ],
+  standalone: true,              
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
